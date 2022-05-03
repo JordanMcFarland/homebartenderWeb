42b65ae46e8d2416c827ac0d7a6d83e36cd37a48
Jordan McFarland <jdmcfarland4591@gmail.com>
2022-05-03 13:38:34 -0500

    Search by Ingredient

--- src/components/CocktailDirectoryComponent.js
+++ src/components/CocktailDirectoryComponent.js
@@ -17,11 +17,13 @@ function RenderDirectoryItem({ cocktail }) {
 
 function CocktailDirectory(props) {
   const [searchTerm, setSearchTerm] = useState('');
+  const [searchBy, setSearchBy] = useState("byName");
   const [cockTailDirectory, setCocktailDirectory] = useState(props.cocktails);
 
   useEffect(() => {
     searchByName();
-    console.log(cockTailDirectory);
+    //console.log(cockTailDirectory);
+    console.log(searchBy);
   }, [searchTerm])
 
   const directory = cockTailDirectory.map(cocktail => {
@@ -34,7 +36,12 @@ function CocktailDirectory(props) {
 
   const searchByName = () => {
     const filteredCocktails = props.cocktails.filter(cocktail => {
-      return cocktail.name.toUpperCase().includes(searchTerm.toUpperCase());
+      if (searchBy === "byName") {
+        return cocktail.name.toUpperCase().includes(searchTerm.toUpperCase());
+      }
+      if (searchBy === "byIngredients") {
+        return cocktail.requiredIngredients.map(ingredient => ingredient.toUpperCase()).includes(searchTerm.toUpperCase());
+      }
     });
     setCocktailDirectory(filteredCocktails);
   }
@@ -42,6 +49,11 @@ function CocktailDirectory(props) {
   return (
     <div className="container py-3">
       <input value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} placeholder="Search" />
+      {' '}
+      <select id="searchBy" onChange={(e) => setSearchBy(e.target.value)}>
+        <option value="byName">By Name</option>
+        <option value="byIngredients">By Ingredients</option>
+      </select>
       {/*<Button onClick={searchByName}>Search</Button> */}
       <div className="row">
         {directory}
