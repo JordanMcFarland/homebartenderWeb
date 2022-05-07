9dbda1f3639cac90ff59a1b5700ac8052ea26bcf
Ross Birdsall <>
2022-05-07 14:30:34 -0400

    feat/ingredient search

--- src/components/CocktailDirectoryComponent.js
+++ src/components/CocktailDirectoryComponent.js
@@ -16,71 +16,105 @@ function RenderDirectoryItem({ cocktail }) {
 }
 
 function CocktailDirectory(props) {
-  const [searchTerm, setSearchTerm] = useState('gin, lemon juice');
-  const [searchBy, setSearchBy] = useState("byName");
-  const [cockTailDirectory, setCocktailDirectory] = useState(props.cocktails);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchBy, setSearchBy] = useState('byName');
+  const [cocktailDirectory, setCocktailDirectory] = useState(props.cocktails);
 
   useEffect(() => {
-    //searchByName();\
-    searchByIngredients();
+    if (searchBy === 'byName') {
+      searchByName();
+    } else {
+      searchByIngredients();
+    }
     //console.log(cockTailDirectory);
-    console.log(searchBy);
-  }, [searchTerm])
+    // console.log(searchBy);
+  }, [searchTerm]);
 
-  const directory = cockTailDirectory.map(cocktail => {
+  const directory = cocktailDirectory.map(cocktail => {
     return (
       <div key={cocktail.id} className="col-md-5 m-1">
         <RenderDirectoryItem cocktail={cocktail} />
       </div>
-    )
+    );
   });
 
   const searchByName = () => {
     const filteredCocktails = props.cocktails.filter(cocktail => {
-      if (searchBy === "byName") {
+      if (searchBy === 'byName') {
         return cocktail.name.toUpperCase().includes(searchTerm.toUpperCase());
       }
-      if (searchBy === "byIngredients") {
-        return cocktail.requiredIngredients.some(ingredient => ingredient.toUpperCase().includes(searchTerm.toUpperCase()));
+      if (searchBy === 'byIngredients') {
+        return cocktail.requiredIngredients.some(ingredient =>
+          ingredient.toUpperCase().includes(searchTerm.toUpperCase())
+        );
       }
     });
     setCocktailDirectory(filteredCocktails);
-  }
+  };
 
   const searchByIngredients = () => {
-    const newSearchTerms = searchTerm.split(', ');
-    console.log(newSearchTerms);
+    // Split words by comma (we will eliminate extra whitespace later)
+    const newSearchTerms = searchTerm.split(',');
 
-    const filteredCocktails = props.cocktails.filter(cocktail => {
-      const arr = [];
-      return newSearchTerms.forEach(term => {
-        const item = cocktail.requiredIngredients.filter(ingredient => ingredient.toUpperCase().includes(term.toUpperCase()));
-        console.log('item;', item);
-      })
+    //empty array to store filtered cocktails
+    const filteredCocktails = [];
 
+    props.cocktails.forEach(cocktail => {
+      // Destructure ingredients out of cocktail object for code readability
+      const { requiredIngredients } = cocktail;
 
+      // Boolean value to indicate whether or not the cocktail includes all required ingredients
+      let hasIngredients = true;
 
+      newSearchTerms.forEach(searchTerm => {
+        // Convert search term to upper case and remove whitespace at beginning and end
+        const term = searchTerm.toUpperCase().trim();
+        // Check each search term to see if there is a cocktail ingredient that contains it
+        if (
+          !requiredIngredients.some(ingredient =>
+            ingredient.toUpperCase().includes(term)
+          )
+        ) {
+          // If not, set the boolean to false to flag that this cocktail should not go in the list
+          hasIngredients = false;
+        }
+      });
+      // Check whether or not all ingredients are included and whether or not the cocktail is already on the list
+      if (
+        hasIngredients &&
+        !filteredCocktails.some(item => item.id === cocktail.id)
+      ) {
+        // If so, pass this into our empty array
+        filteredCocktails.push(cocktail);
+      }
+    });
 
-
-      // return cocktail.requiredIngredients.some(ingredient => newSearchTerms.some(searchTerm => {
-      //   return searchTerm.toUpperCase().includes(ingredient.toUpperCase());
-      // }));
-    })
+    // Finally, set state with the array of cocktails
     setCocktailDirectory(filteredCocktails);
-  }
+
+    // return newSearchTerms.forEach(term => {
+    //   const item = cocktail.requiredIngredients.filter(ingredient => ingredient.toUpperCase().includes(term.toUpperCase()));
+    //   console.log('item;', item);
+    // })
+
+    // return cocktail.requiredIngredients.some(ingredient => newSearchTerms.some(searchTerm => {
+    //   return searchTerm.toUpperCase().includes(ingredient.toUpperCase());
+    // }));
+  };
 
   return (
     <div className="container py-3">
-      <input value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} placeholder="Search" />
-      {' '}
-      <select id="searchBy" onChange={(e) => setSearchBy(e.target.value)}>
+      <input
+        value={searchTerm}
+        onChange={e => setSearchTerm(e.target.value)}
+        placeholder="Search"
+      />{' '}
+      <select id="searchBy" onChange={e => setSearchBy(e.target.value)}>
         <option value="byName">By Name</option>
         <option value="byIngredients">By Ingredients</option>
       </select>
       {/*<Button onClick={searchByName}>Search</Button> */}
-      <div className="row">
-        {directory}
-      </div>
+      <div className="row">{directory}</div>
     </div>
   );
 }
